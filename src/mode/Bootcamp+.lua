1406f84ec6a7343b451eb4811d6887df71e3a7f7
Lautenschlager <tai.romani@hotmail.com>
2017-06-03 14:44:11 -0300

    Update Bootcamp+.lua

--- src/mode/Bootcamp+.lua
+++ src/mode/Bootcamp+.lua
@@ -182,7 +182,7 @@ mode.bootcampP = {
 		system.bindKeyboard(n,46,true,true) -- Delete key
 		tfm.exec.chatMessage("<T>" .. system.getTranslation("welcome") .. "\n\t<CEP>/w Mquk #bootcamp+ @mapCode",n)
 		
-		local id = tfm.exec.addImage("15c6ee6324d.png","&0",400,200,n)
+		local id = tfm.exec.addImage("15c6ee6324d.png","&0",120,100,n)
 		system.newTimer(function()
 			tfm.exec.removeImage(id)
 		end,5000,false)
