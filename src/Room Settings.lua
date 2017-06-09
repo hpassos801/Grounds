ac63d7268366c2d312e5448d7c0bf4b7e2e9e840
Lautenschlager <tai.romani@hotmail.com>
2017-06-09 16:37:57 -0300

    Create Room Settings.lua

--- src/Room Settings.lua
+++ src/Room Settings.lua
@@ -11,7 +11,28 @@ system.roomSettings = {
 		if not game then
 			system.gameMode = "grounds"
 		end
-	end
+	end,
+	["vanilla"] = function()
+		system.officialModeMessage = "<VP>Enjoy your vanilla (: .. okno"
+	end,
+	["survivor"] = function()
+		system.officialModeMessage = "<R>Aw, you cannot play survivor on #grounds"
+	end,
+	["racing"] = function()
+		system.officialModeMessage = "<CH>Uh, racing? Good luck!"
+	end,
+	["music"] = function()
+		system.officialModeMessage = "<BV>Music? Nice choice! Why don't you try a rock'n'roll?"
+	end,
+	["bootcamp"] = function()
+		system.officialModeMessage = "<PT>Bootcamp? Ok. This is unfair and your data won't be saved out of the room."
+	end,
+	["defilante"] = function()
+		system.officialModeMessage = "<R>Aw, you cannot play defilante on #grounds"
+	end,
+	["village"] = function()
+		system.officialModeMessage = "<R>You cannot play village on #grounds. Please, change your room."
+	end,
 }
 system.setRoom = function()
 	if system.isRoom and system.roomAttributes then
